b737239d4d94d3aaba30550cba4d7f19d87cc37f
githubBobur <bobur.shoh.94@mail.ru>
2016-03-30 14:42:39 +0600

    asd

---
+++ week6/Mouse_click_event/Mouse_click_event/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mouse_click_event
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(e.Location.ToString());
+            Console.WriteLine(e.Location.ToString());
+        }
+    }
+}
